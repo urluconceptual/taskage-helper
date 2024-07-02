91cd2ca824f4c80b7efc580d19f9fec4c6d019ec
Alexandra-Diana Ciocan <ciocan_alexandra16@yahoo.com>
2024-07-02 20:00:50 +0300

    fix: env variables

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 
 from controller.task_assignment import task_assignment_blueprint
 from database import db
 
+load_dotenv()
 app = Flask(__name__)
 
 CORS(app, resources={r"/assignment/*": {"origins": os.getenv('ALLOWED_ORIGINS')}})
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db.init_app(app)
 
 app.register_blueprint(task_assignment_blueprint, url_prefix='/helper/assignment')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='http://0.0.0.0', port="5000")
